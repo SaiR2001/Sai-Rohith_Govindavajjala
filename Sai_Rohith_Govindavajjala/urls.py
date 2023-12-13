1c66195976083dea0c89ebff5ae0bab07f005f45
Sai Rohith Govindavajjala <govindavajjalasairohith@gmail.com>
2023-12-13 14:41:01 -0800

    commit3

--- Sai_Rohith_Govindavajjala/urls.py
+++ Sai_Rohith_Govindavajjala/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contacts/', include('contacts.urls')),
-    path('', include('contacts.urls')),  # Adding this line if you want contacts/ to be the default page
+    path('', include('contacts.urls')), 
 ]
